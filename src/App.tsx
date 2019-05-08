4ae1e9d0688eded5655c71854473e5855b29dd89
Olena Fedorets <Olena_Fedorets@epam.com>
2019-05-08 13:41:55 +0300

    feat: Initialized project

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Header from './Header';
+
+import './App.scss';
+
+function App() {
+  return (
+      <div>
+        <Header />
+
+        <div className="container py-4">
+          beginning...
+        </div>
+      </div>
+  );
+}
+
+export default App;
+
