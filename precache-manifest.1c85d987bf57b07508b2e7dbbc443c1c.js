32f89975de3552e6cc7fa5805471de1cf1166d35
Olena Fedorets <Olena_Fedorets@epam.com>
2019-05-15 13:44:25 +0300

    Updates

---
+++ precache-manifest.1c85d987bf57b07508b2e7dbbc443c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a104b56407c727d5bca9efc05e659cbf",
+    "url": "/Uflixit/index.html"
+  },
+  {
+    "revision": "f38b060a167c067ce278",
+    "url": "/Uflixit/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3c13371a282939c3908e",
+    "url": "/Uflixit/static/css/main.0fd89b1c.chunk.css"
+  },
+  {
+    "revision": "f38b060a167c067ce278",
+    "url": "/Uflixit/static/js/2.b1c2f6e9.chunk.js"
+  },
+  {
+    "revision": "3c13371a282939c3908e",
+    "url": "/Uflixit/static/js/main.f2488cae.chunk.js"
+  },
+  {
+    "revision": "77bbd11af162102dcfaf",
+    "url": "/Uflixit/static/js/runtime~main.58f247f9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9a2417504076abf66f3ad5055f36d1b.js"
+  "/Uflixit/precache-manifest.1c85d987bf57b07508b2e7dbbc443c1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Uflixit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
